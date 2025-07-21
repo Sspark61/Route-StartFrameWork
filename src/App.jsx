e2cb3e30d77453bc6b911f7b71b8109cf244318c
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2025-07-21 17:14:30 +0300

    Refactor App component to implement routing with React Router and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import Portfolio from './Portfolio/Portfolio';
+import Contact from './Contact/Contact';
+import Layout from './Layout/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout/>,
+      children: [
+          {
+            path: "",
+            element: <Home />,
+          },
+          {
+            path: "about",
+            element: <About />,
+          },
+          {
+            path: "portfolio",
+            element: <Portfolio />,
+          },
+          {
+            path: "contact",
+            element: <Contact />,
+          },
+        ]
+      }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
