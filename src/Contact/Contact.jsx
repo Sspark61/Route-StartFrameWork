f69f0577d1673ee773574d5691c70c10eac8dea8
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2025-07-21 17:16:42 +0300

    feat: add Contact, Home, and Portfolio components

---
+++ src/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      contact page
+      Here you can reach out to me for any inquiries or collaborations.
+    </div>
+  )
+}
---
+++ src/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+    <div>
+      This is my Portfolio component.
+      Here you can find my projects and works.
+    </div>
+  )
+}
